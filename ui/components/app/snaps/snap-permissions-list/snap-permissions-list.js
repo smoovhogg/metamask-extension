8799a95966d956ae74dafd8260e07fad0cf9f787
david0xd <david@timechaser.org>
2024-05-17 12:40:05 +0200

    Refactor components structure

--- ui/components/app/snaps/snap-permissions-list/snap-permissions-list.js
+++ ui/components/app/snaps/snap-permissions-list/snap-permissions-list.js
@@ -1,36 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
-import { SubjectType } from '@metamask/permission-controller';
 import { getWeightedPermissions } from '../../../../helpers/utils/permission';
 import { useI18nContext } from '../../../../hooks/useI18nContext';
-import PermissionCell from '../../permission-cell';
-import {
-  AvatarBase,
-  AvatarBaseSize,
-  AvatarFavicon,
-  AvatarFaviconSize,
-  Box,
-  IconSize,
-  Text,
-} from '../../../component-library';
+import { Box } from '../../../component-library';
 import {
   getMultipleTargetsSubjectMetadata,
   getSnapsMetadata,
 } from '../../../../selectors';
-import {
-  getAvatarFallbackLetter,
-  getSnapName,
-} from '../../../../helpers/utils/util';
-import {
-  AlignItems,
-  BackgroundColor,
-  Display,
-  FontWeight,
-  JustifyContent,
-  TextColor,
-  TextVariant,
-} from '../../../../helpers/constants/design-system';
+import { getSnapName } from '../../../../helpers/utils/util';
+import SnapPermissionCellDisplay from '../snap-permission-cell-display';
 
 export default function SnapPermissionsList({
   snapId,
@@ -49,102 +28,6 @@ export default function SnapPermissionsList({
     getMultipleTargetsSubjectMetadata(state, connections),
   );
 
-  const showPermissionCell = (permission, index) => {
-    if (permission.connection) {
-      const targetSubjectMetadata =
-        targetSubjectsMetadata[permission.connection];
-      let title = permission.label;
-      let { description } = permission;
-
-      if (targetSubjectMetadata?.subjectType === SubjectType.Snap) {
-        title = t('snapConnectTo', [
-          <Text
-            key="connectToMain"
-            fontWeight={FontWeight.Medium}
-            variant={TextVariant.inherit}
-            color={TextColor.inherit}
-          >
-            {targetSubjectMetadata.name}
-          </Text>,
-        ]);
-        description = t('snapConnectionPermissionDescription', [
-          <Text
-            key={`permissionSubject_${permission.subjectName}`}
-            fontWeight={FontWeight.Medium}
-            variant={TextVariant.inherit}
-            color={TextColor.inherit}
-          >
-            {permission.subjectName}
-          </Text>,
-          <Text
-            key={`permissionSubjectDescription_${targetSubjectMetadata.name}`}
-            fontWeight={FontWeight.Medium}
-            variant={TextVariant.inherit}
-            color={TextColor.inherit}
-          >
-            {targetSubjectMetadata.name}
-          </Text>,
-        ]);
-      }
-
-      const faviconUrl = targetSubjectMetadata?.iconUrl;
-
-      const permissionIcon = faviconUrl ? (
-        <AvatarFavicon
-          backgroundColor={BackgroundColor.backgroundAlternative}
-          size={AvatarFaviconSize.Md}
-          iconProps={{
-            size: IconSize.Sm,
-          }}
-          src={faviconUrl}
-          name={permission.connection}
-        />
-      ) : (
-        <AvatarBase
-          size={AvatarBaseSize.Md}
-          display={Display.Flex}
-          alignItems={AlignItems.center}
-          justifyContent={JustifyContent.center}
-          color={TextColor.textAlternative}
-          style={{ borderWidth: '0px' }}
-          backgroundColor={BackgroundColor.backgroundAlternative}
-        >
-          {getAvatarFallbackLetter(permission.connectionName)}
-        </AvatarBase>
-      );
-
-      return (
-        <PermissionCell
-          snapId={snapId}
-          permissionName={permission.permissionName}
-          title={title}
-          description={description}
-          weight={permission.weight}
-          avatarIcon={permissionIcon}
-          dateApproved={permission?.permissionValue?.date}
-          key={`${permission.permissionName}-${index}`}
-          showOptions={showOptions}
-          connection={permission.connection}
-        />
-      );
-    }
-
-    return (
-      <PermissionCell
-        snapId={snapId}
-        permissionName={permission.permissionName}
-        title={permission.label}
-        description={permission.description}
-        weight={permission.weight}
-        avatarIcon={permission.leftIcon}
-        dateApproved={permission?.permissionValue?.date}
-        key={`${permission.permissionName}-${index}`}
-        showOptions={showOptions}
-        connection={permission.connection}
-      />
-    );
-  };
-
   return (
     <Box className="snap-permissions-list">
       {getWeightedPermissions({
@@ -152,7 +35,16 @@ export default function SnapPermissionsList({
         permissions: permissionsToShow,
         subjectName: snapName,
         getSubjectName: getSnapName(snapsMetadata),
-      }).map((permission, index) => showPermissionCell(permission, index))}
+      }).map((permission, index) => (
+        <SnapPermissionCellDisplay
+          snapId={snapId}
+          showOptions={showOptions}
+          subjectMetadata={targetSubjectsMetadata[permission.connection]}
+          permission={permission}
+          index={index}
+          key={`permissionCellDisplay_${snapId}_${index}`}
+        />
+      ))}
     </Box>
   );
 }
